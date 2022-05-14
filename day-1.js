25a1c119850e6e2d56bfbf417e42a875e9135a02
Khasanov-A <Akhmadjon5211@gmail.com>
2022-05-14 17:02:36 +0500

    Arifmetik ammallarni tugatdim.

---
+++ day-1.js
@@ -0,0 +1,62 @@
+/* ========== Arithmetic Operators ========== */
+
+//Addition
+let a = 1
+let b = 0
+let c = "5"
+let d = false
+let e = 'a'
+
+// add
+
+// console.log("a + b = ", a + b);
+// console.log("a + c = ", a + c);
+// console.log("a + d = ", a + d);
+// console.log("a + e = ", a + e);
+// console.log("b + c = ", b + c);
+// console.log("b + d = ", b + d);
+// console.log("b + e = ", b + e);
+// console.log("c + d = ", c + d);
+// console.log("c + e = ", c + e);
+// console.log("d + e = ", d + e);
+
+// console.log("/n") yangi qator ochib beradi "open new line"
+
+// subtraction
+
+// console.log("a - b = ", a - b);
+// console.log("a - c = ", a - c);
+// console.log("a - d = ", a - d);
+// console.log("a - e = ", a - e);
+// console.log("b - c = ", b - c);
+// console.log("b - d = ", b - d);
+// console.log("b - e = ", b - e);
+// console.log("c - d = ", c - d);
+// console.log("c - e = ", c - e);
+// console.log("d - e = ", d - e);
+
+// multiplication
+
+// console.log("a * b = ", a * b);
+// console.log("a * c = ", a * c);
+// console.log("a * d = ", a * d);
+// console.log("a * e = ", a * e);
+// console.log("b * c = ", b * c);
+// console.log("b * d = ", b * d);
+// console.log("b * e = ", b * e);
+// console.log("c * d = ", c * d);
+// console.log("c * e = ", c * e);
+// console.log("d * e = ", d * e);
+
+// divide
+
+console.log("a / b = ", a / b);
+console.log("a / c = ", a / c);
+console.log("a / d = ", a / d);
+console.log("a / e = ", a / e);
+console.log("b / c = ", b / c);
+console.log("b / d = ", b / d);
+console.log("b / e = ", b / e);
+console.log("c / d = ", c / d);
+console.log("c / e = ", c / e);
+console.log("d / e = ", d / e);
