11d12c2c0f04d081c389b72db0e61764b712b549
Khasanov-A <Akhmadjon5211@gmail.com>
2022-05-15 11:00:14 +0500

    Finished day 1

---
+++ day_1_exercises.js
@@ -0,0 +1,91 @@
+// 1) Write a single line comment which says, comments can make code readable
+
+// Hello World!
+
+// 2) Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// welcome to 30 DaysofJavaScript
+
+// 3) Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/* 
+let firstName = Ahmadjon
+let lastName = Xasanov
+let age = 25
+let country = Uzbekiston
+let city = Urgench
+*/
+
+// 4) Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let a = 1
+let b = 0
+let c = true
+let d = false
+let e = 3.14
+let f = 'tim'
+let g = '3'
+console.log(a + d);
+console.log(a * b);
+
+// 5) Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof(g + d));
+console.log(typeof(g + b));
+console.log(typeof(g + c));
+console.log(typeof(g + f));
+
+console.log(typeof(g - d));
+console.log(typeof(g - b));
+console.log(typeof(g - c));
+console.log(typeof(g - f));
+
+console.log(typeof(g * d));
+console.log(typeof(g * b));
+console.log(typeof(g * c));
+console.log(typeof(g * f));
+
+console.log(typeof(g / d));
+console.log(typeof(g / b));
+console.log(typeof(g / c));
+console.log(typeof(g / f));
+
+// 6) Declare four variables without assigning values
+
+console.log(a / f);
+console.log(g / f);
+console.log(g * f);
+console.log(b - f);
+
+// 7) Declare four variables with assigned values
+
+console.log(a + g);
+console.log(g + f);
+console.log(g - d);
+console.log(b / a);
+
+// 8) Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName = 'Akhmadjon'
+let lastName = 'Khasanov'
+let maritalStatus = 'juda yaxshi'
+let country = 'Uzbekistan'
+let age = '25 years old'
+
+console.log('firstName = ' + firstName);
+console.log('lastName = ' + lastName);
+console.log('maritalStatus = ' + maritalStatus);
+console.log('country = ' + country);
+console.log('age = ' + age);
+
+// 9) Declare variables to store your first name, last name, marital status, country and age in a single line
+
+console.log(firstName + ' ' + lastName + ' ' + maritalStatus + ' ' + country + ' ' + age);
+
+// 10) Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 'I am 25 years old'
+let yourAge = 'You are 23 years old'
+
+console.log(myAge);
+console.log(yourAge);
